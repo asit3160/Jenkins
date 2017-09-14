96d00a1700079f1f7e69a7e8038ce3b6e2a64dbc
asit3160 <31497382+asit3160@users.noreply.github.com>
2017-09-14 08:48:09 +0530

    2nd msz commit

--- src/GITBash/GitHub.java
+++ src/GITBash/GitHub.java
@@ -5,6 +5,28 @@ public class GitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		System.out.println("Asit Kumar Prusty");
+		
+		
+		
+		
+		
+		
 	}
+	
+	
 
 }
